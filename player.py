bab0a8bc4b9dcb150a66cea30887af2398251d10
tomson <tomsonj88@wp.pl>
2022-04-04 23:52:03 +0200

    Add make_decision draft method by human

--- player.py
+++ player.py
@@ -17,6 +17,10 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.deck = Deck()
+        self.cards = self.take_cards(2)
+
+    def __str__(self):
+        return f"{self.name} has cards: {self.cards}. Points: {self.count_points()}"
 
     def take_cards(self, quantity):
         if quantity < 0 or quantity > 52:
@@ -28,11 +32,12 @@ class Player:
             quantity -= 1
         return cards
 
-    def stop(self):
-        pass
 
-    @staticmethod
-    def count_points(cards):
+    def turn_pass(self):
+        print("PASS")
+        return "pass"
+
+    def count_points(self):
         point = 0
         point_dict = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10,
                       "K": 10, "A": 1}
@@ -40,8 +45,25 @@ class Player:
         rest_values = ['J', 'Q', 'K', 'A']
         [values.append(element) for element in rest_values]
 
-        for element in cards:
+        for element in self.cards:
             for value in values:
                 if element == Card(value):
                     point += point_dict[value]
         return point
+
+
+class Human(Player):
+    def make_decision(self):
+        decision = input("Make decision: t - take another cards or p - pass")
+        if decision == "t":
+            self.cards.append(self.take_cards(1)[0])
+            print("dupa")
+        elif decision == "p":
+            self.turn_pass()
+        else:
+            print("bad choice")
+
+
+class Dealer(Player):
+    # ToDo: make here dealer game algorithm
+    pass
